96e4697424ec9d50b1e1b9a8c366475defc74204
Ryan Shouse <ryan.shouse@mykenzie.snhu.edu>
2023-08-14 14:13:33 -0500

    Lots of changes, needs responsiveness for desktop

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,36 @@ for(i=0; i<collapsible.length; i++) {
         }
     })
 }
+
+let checkbox = document.getElementById("checkbox");
+let body = document.body;
+let navBar = document.getElementById("nav-bar");
+let makeBig = document.querySelectorAll(".make-big");
+let projectButton = document.querySelectorAll(".collapsible");
+let dropButton = document.getElementById("drop-button");
+let navLinks = document.getElementById("nav-links");
+let navA = document.querySelectorAll(".nav-a");
+
+checkbox.addEventListener("change", function(){
+    body.classList.toggle("dark");
+    navBar.classList.toggle("dark");
+    navLinks.classList.toggle("dark");
+    dropButton.classList.toggle("dark");
+    for(each of makeBig) {
+        each.classList.toggle("dark");
+    }
+    for(each of projectButton) {
+        each.classList.toggle("dark");
+    }
+})
+
+dropButton.addEventListener("click", function(){
+    navLinks.classList.toggle("display");
+})
+
+
+for(each of navA) {
+    each.addEventListener("click", function(){
+        navLinks.classList.toggle("display");
+    })
+}
