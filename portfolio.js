c8d7974a9fcdb365d2fd1b7ef87f25ecb39fac85
Ryan Shouse <ryan.shouse@mykenzie.snhu.edu>
2023-08-19 12:57:52 -0500

    fixed nav menu issue on big screen

--- portfolio.js
+++ portfolio.js
@@ -14,15 +14,32 @@ for(i=0; i<collapsible.length; i++) {
 
 let dropButton = document.getElementById("drop-button");
 let navLinks = document.getElementById("nav-links");
-let navA = document.querySelectorAll(".nav-a");
+let navA = document.querySelectorAll(".nav-a"); 
 
-dropButton.addEventListener("click", function(){
+let smallScreen = window.matchMedia("(max-width: 530px)");
+
+let addDropMenu = function() {
     navLinks.classList.toggle("display");
-})
-
-
-for(each of navA) {
-    each.addEventListener("click", function(){
-        navLinks.classList.toggle("display");
-    })
 }
+
+let screenSizeChange = function(mm) {
+    if (mm.matches) {
+        dropButton.addEventListener("click", addDropMenu);
+    
+    
+        for(each of navA) {
+            each.addEventListener("click", addDropMenu);
+        }
+    }
+    else {
+        dropButton.removeEventListener("click", addDropMenu);
+
+        for(each of navA) {
+            each.removeEventListener("click", addDropMenu);
+        }
+    }
+}
+
+screenSizeChange(smallScreen);
+
+smallScreen.addEventListener("change", screenSizeChange);
